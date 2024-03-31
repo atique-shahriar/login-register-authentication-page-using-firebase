7569a3a38608a9a90a4649e8eef19fddba8b48fa
atique-shahriar <satique06@gmail.com>
2024-03-31 16:24:06 +0600

    Setup project and create components

---
+++ src/components/firebase.config.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBECrH3l8Ba_gO8FmjG0LBerQKzO2XPbNc",
+    authDomain: "login-register-page-cd501.firebaseapp.com",
+    projectId: "login-register-page-cd501",
+    storageBucket: "login-register-page-cd501.appspot.com",
+    messagingSenderId: "159333007094",
+    appId: "1:159333007094:web:fef39e9b31fa51e6c5f6d7"
+};
+
+const app = initializeApp(firebaseConfig);
