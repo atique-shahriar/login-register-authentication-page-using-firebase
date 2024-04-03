71e0c624d088dafd055b73b429025af92762a396
atique-shahriar <satique06@gmail.com>
2024-04-04 00:17:48 +0600

    Create user from register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,34 +1,62 @@
-import {Link} from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { auth } from "./firebase.config";
 
 const Register = () => {
+  const [createUserError, setCreateUserError] = useState("");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const displayName = e.target.username.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const confirmPassword = e.target.confirmPassword.value;
+    console.log(createUserError);
+    setCreateUserError("");
+    console.log(createUserError);
+    console.log(displayName, email, password, confirmPassword);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        setCreateUserError(error.message);
+      });
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-r from-[#ED765E] to-[#FEA858]">
       <div className="flex items-center justify-center min-h-screen">
         <div className="bg-white w-4/5 lg:w-1/3 rounded-lg mx-auto p-10 h-5/6 ">
           <h3 className="text-center font-bold text-4xl mb-10 ">Register</h3>
-          <label htmlFor="text" className="block mt-6">
-            Username
-          </label>
-          <input className="block px-5 py-2  border-b-2 w-full mt-2 " type="email" placeholder="Type username" />
+          <form onSubmit={handleRegister}>
+            <label htmlFor="text" className="block mt-6">
+              Username
+            </label>
+            <input className="block px-5 py-2  border-b-2 w-full mt-2 " name="username" type="text" placeholder="Type username" />
 
-          <label htmlFor="email" className="block mt-6">
-            Email
-          </label>
-          <input className="block px-5 py-2  border-b-2 w-full mt-2 " type="email" placeholder="Type email address" />
+            <label htmlFor="email" className="block mt-6">
+              Email
+            </label>
+            <input className="block px-5 py-2  border-b-2 w-full mt-2 " name="email" type="email" placeholder="Type email address" />
 
-          <label htmlFor="email" className="block mt-6">
-            Password
-          </label>
-          <input className="block px-5 py-2 border-b-2 w-full mt-2" type="password" placeholder="Type password" />
+            <label htmlFor="email" className="block mt-6">
+              Password
+            </label>
+            <input className="block px-5 py-2 border-b-2 w-full mt-2" name="password" type="password" placeholder="Type password" />
 
-          <label htmlFor="email" className="block mt-6">
-            Confirm Password
-          </label>
-          <input className="block px-5 py-2 border-b-2 w-full mt-2" type="password" placeholder="Type password again" />
+            <label htmlFor="email" className="block mt-6">
+              Confirm Password
+            </label>
+            <input className="block px-5 py-2 border-b-2 w-full mt-2" name="confirmPassword" type="password" placeholder="Type password again" />
 
-          <div className="flex mt-4 justify-center">
-            <input type="submit" value="REGISTER" className="bg-gradient-to-r from-[#ED765E] to-[#FEA858] w-3/4 py-2 rounded-xl font-bold text-white" />
-          </div>
+            <div className="flex mt-4 justify-center">
+              <input type="submit" value="REGISTER" className="bg-gradient-to-r from-[#ED765E] to-[#FEA858] w-3/4 py-2 rounded-xl font-bold text-white" />
+            </div>
+          </form>
           <div className="flex flex-col items-center justify-center  mt-6">
             <p className="mt-4">
               Are have an account?{" "}
@@ -37,6 +65,7 @@ const Register = () => {
               </span>
             </p>
           </div>
+          <ToastContainer />
         </div>
       </div>
     </div>
--- src/components/firebase.config.js
+++ src/components/firebase.config.js
@@ -1,13 +1,14 @@
-
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBECrH3l8Ba_gO8FmjG0LBerQKzO2XPbNc",
-    authDomain: "login-register-page-cd501.firebaseapp.com",
-    projectId: "login-register-page-cd501",
-    storageBucket: "login-register-page-cd501.appspot.com",
-    messagingSenderId: "159333007094",
-    appId: "1:159333007094:web:fef39e9b31fa51e6c5f6d7"
+  apiKey: "AIzaSyBECrH3l8Ba_gO8FmjG0LBerQKzO2XPbNc",
+  authDomain: "login-register-page-cd501.firebaseapp.com",
+  projectId: "login-register-page-cd501",
+  storageBucket: "login-register-page-cd501.appspot.com",
+  messagingSenderId: "159333007094",
+  appId: "1:159333007094:web:fef39e9b31fa51e6c5f6d7",
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
